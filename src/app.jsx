08236449044f83deba6ac10fe678d5772f819cc3
viktor@scheglov.pro <viktor@scheglov.pro>
2015-10-24 23:55:48 +0300

    pure css framework and initialized REACT

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,12 @@ var React = require('react');
 
 var Hello = React.createClass({
   render: function() {
-<<<<<<< HEAD
-    return <h1 onClick={this.helperInfo()} className="red">
-      Hello!
+    return <h1>
+      Hello
     </h1>
-  },
-  helperInfo: function() {
-  	console.log(this);
-=======
-    return <h1 className="red">
-      Hello!
-    </h1>
->>>>>>> 3e2840c4ce2a16d449bd123b4ec3b1a6bf5b1a34
   }
 });
 
+
 var element = React.createElement(Hello, {});
-React.render(element, document.querySelector('.container'));
+React.render(element, document.querySelector('.content'));
