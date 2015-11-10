1d956c2f3bafb53d2d6933592367eeb138e79cd0
viktor@scheglov.pro <viktor@scheglov.pro>
2015-11-10 23:29:56 +0300

    login start auth

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,12 +4,15 @@ var HashHistory = require('react-router/lib/hashhistory');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
-
+var Login = require('./components/login');
 var Main = require('./components/main');
 
 module.exports = (
   <Router history={new HashHistory}>
   	<Route path="/" component={Main}>
   	</Route>
+  	<Route path="/login" component={Login}>
+  	</Route>
+
   </Router>
 )
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -8,5 +8,20 @@ module.exports = {
     .then(function(response){
       return response.json()
     })
-  }
+  },
+  'facebookAuth' : {
+        'clientID'      : 'your-secret-clientID-here', // your App ID
+        'clientSecret'  : 'your-client-secret-here', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+   },
+   'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+   },
+   'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+   }
 };
