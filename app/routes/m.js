ddb80c6cb00cc7616fdc798c1ceb30c0ebc46fd0
Viktor Scheglov <viktor@scheglov.pro>
2016-09-20 19:09:33 +0300

    add mistake router

--- app/routes/m.js
+++ app/routes/m.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var rdb = require('../db/database');
 var router = express.Router();
+var auth = require('../db/auth');
 
-router.post('/', function (req, res, next) {
-  rdb.save('m', {id: 1})
+
+router.get('/', auth.authorize, function (req, res) {
+    rdb.findAll('m')
     .then(function (m) {
-    	res.json(m);
-    })
+        res.json(m);
+    });
 });
+
+router.post('/', auth.authorize, function (req, res) {
+        var newM = {
+            m: req.body.m,
+            left: req.body.left,
+            right: req.body.right,
+            url: req.body.right,
+            account: req.body.site
+        };
+
+        rdb.save('m', newM)
+        .then(function (result) {
+            res.json(result);
+        });
+});
+
 
 module.exports = router;
