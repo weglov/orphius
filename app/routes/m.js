1123ad552aa711a2c4bfe4f943894cef8cf42d25
Viktor Scheglov <viktor@scheglov.pro>
2016-09-21 13:08:17 +0300

    API mistake action

--- app/routes/m.js
+++ app/routes/m.js
@@ -3,28 +3,66 @@ var rdb = require('../db/database');
 var router = express.Router();
 var auth = require('../db/auth');
 
+var table = 'm';
 
-router.get('/', auth.authorize, function (req, res) {
-    rdb.findAll('m')
+router.get('/:resource', auth.authorize, function (req, res, next) {
+    rdb.findIndexed(table, req.params.resource, 'resource')
     .then(function (m) {
+        if(!m) {
+            var notFoundError = new Error('Resource not found');
+            notFoundError.status = 404;
+            return next(notFoundError);
+        }
         res.json(m);
     });
 });
 
-router.post('/', auth.authorize, function (req, res) {
-        var newM = {
+
+router.post('/', function (req, res) {
+		if (req.body.m && req.body.url && req.body.resource) {
+			var newM = {
             m: req.body.m,
             left: req.body.left,
             right: req.body.right,
-            url: req.body.right,
-            account: req.body.site
-        };
+            check: req.body.check || false,
+            url: req.body.url,
+            resource: req.body.resource
+        	};
+		} else {
+			var incorrectError = new Error('incorrect mistake');
+            incorrect.status = 401;
+            return next(incorrect);
+		}
+        
 
-        rdb.save('m', newM)
+        rdb.save(table, newM)
         .then(function (result) {
             res.json(result);
         });
 });
+
+router.put('/:id', auth.authorize, function (req, res) {
+    rdb.find(table, req.params.id)
+    .then(function (m) {
+        var updateM = {
+            check: req.body.check || m.check,
+        };
+
+        rdb.edit(table, check.id, updateUser)
+        .then(function (results) {
+            res.json(results);
+        });
+    });
+});
+
+router.delete('/:id', auth.authorize, function (req, res) {
+    rdb.destroy(table, req.params.id)
+    .then(function (results) {
+        res.json(results);
+    });
+});
+
+
 
 
 module.exports = router;
