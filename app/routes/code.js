ec9015c05595ae11407e855c06fe94a8165ece36
Viktor Scheglov <viktor@scheglov.pro>
2016-10-26 16:32:14 +0300

    code v1 done

--- app/routes/code.js
+++ app/routes/code.js
@@ -1,110 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
-
+var rdb = require('../db/database');
+var config = require('../../config.js');
+var code = require('./code/v1.js');
 // Get information about a resource. 
-router.get('/v1/:id',  function (req, res, next) {
-    res.setHeader('Content-Type', 'text/javascript; charset=UTF-8');
-    res.status(200).send(`
-  window.orphio = window.orphio || function() {
-  var options = {};
-  var defaultOptions = {
-    url: 'example.php',
-    callback: function() {
-      alert('Thank you! Misprint was sent');
-    },
-    userId: 0,
-    maxLength: 1000,
-    minLength: 4
-  };
-
-  var ajaxPost = function(url, data, callback) {
-    if (window.XMLHttpRequest == undefined) {
-      return;
+router.get('/:id.js',  function (req, res, next) {
+    // Setting code
+    var init = {
+      resource: '',
+      url: config.url + 'm'
     }
-
-    var request = new XMLHttpRequest();
-    request.open('POST', url, true);
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4) {
-        if (this.status >= 200 && this.status < 400) {
-          var resp = this.responseText;
-          if (callback != undefined) {
-            callback(resp);
-          }
-        } else {
-          // error callback
+    
+    // Полученние настроек ресурса
+    rdb.find("resource", req.params.id)
+      .then(function (resource) {
+        init.resource = resource.id
+        return resource;
+    }).then(function(resource) {
+        var data = {
+          url: config.url + 'm',
+          resource: resource.id
         }
-      }
-    };
+        res.setHeader('Content-Type', 'text/javascript; charset=UTF-8');
+        res.status(200).send(code(JSON.stringify(data)));
+    })
 
-    var query = [];
-    for (var key in data) {
-      query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-    }
-
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(query.join('&'));
-    request = null;
-  }
-
-  var getSelectedText = function() {
-    var selection = window.getSelection();
-    var range = selection.getRangeAt(0);
-    var padding = 30;
-    var leftStartIdx = range.startOffset - padding;
-    var leftEndIdx   = range.startOffset;
-    var left  = (range.startContainer.textContent || "").substring(leftStartIdx, leftEndIdx); 
-    var rightStartIdx = range.endOffset;
-    var rightEndIdx   = range.endOffset + padding;
-    var rightContent  = range.endContainer.textContent || "";
-    var right = rightContent.substring(rightStartIdx, rightEndIdx);
-    return {
-      left: left,
-      emph: selection.toString(),
-      right: right
-    }
-  }
-
-  var onKeyPress = function() {
-    var e = arguments[0] || window.event;
-    var code = e.keyCode ? e.keyCode : (e.which ? e.which : e.charCode);
-
-    if (e.ctrlKey && (code == 13 || code == 10)) {
-        console.log(getSelectedText())
-        // sendReport(getSelectedText());
-    }
-  }
-
-  var sendReport = function(text) {
-    if (text !== undefined && text.length <= options.maxLength && text.length >= options.minLength) {
-      options.callback();
-
-      ajaxPost(options.url, {misprintText: text, misprintUrl: window.location.href ? window.location.href : '', misprintUserId: options.userId});
-    }
-  }
-
-  var init = function(opts) {
-    options.url = opts.url || defaultOptions.url;
-    options.callback = opts.callback || defaultOptions.callback;
-    options.userId = opts.userId || defaultOptions.userId;
-    options.minLength = opts.minLength || defaultOptions.minLength;
-    options.maxLength = opts.maxLength || defaultOptions.maxLength;
-
-    document.onkeypress = onKeyPress;
-  }
-
-  return {
-    'init': init
-  }
-}();
-try {
-  orphio.init({url: ''})
-} catch (e) {
-  console.log(e);
-}
-`);
+   
 
 });
 
--- app/routes/code/v1.js
+++ app/routes/code/v1.js
@@ -1,98 +1,116 @@
-  window.orphio = window.orphio || function() {
-  var options = {};
-  var defaultOptions = {
-    url: 'example.php',
-    callback: function() {
-      alert('Thank you! Misprint was sent');
-    },
-    userId: 0,
-    maxLength: 1000,
-    minLength: 4
-  };
+module.exports = function (data) {  
+    return `
+    window.orphio = window.orphio || function() {
+    var options = {};
 
-  var ajaxPost = function(url, data, callback) {
-    if (window.XMLHttpRequest == undefined) {
-      return;
-    }
-
-    var request = new XMLHttpRequest();
-    request.open('POST', url, true);
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4) {
-        if (this.status >= 200 && this.status < 400) {
-          var resp = this.responseText;
-          if (callback != undefined) {
-            callback(resp);
-          }
-        } else {
-          // error callback
-        }
-      }
+    var defaultOptions = {
+      url: '',
+      callback: function() {
+        console.log('Thank you! Misprint was sent');
+      },
+      maxLength: 80,
+      minLength: 4,
+      resource: ''
     };
 
-    var query = [];
-    for (var key in data) {
-      query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    var ajaxPost = function(url, data, callback) {
+      if (window.XMLHttpRequest == undefined) {
+        return;
+      }
+
+      var request = new XMLHttpRequest();
+      request.open('POST', url, true);
+
+      request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+          if (this.status >= 200 && this.status < 400) {
+            var resp = this.responseText;
+            if (callback != undefined) {
+              callback(resp);
+            }
+          } else {
+            // error callback
+          }
+        }
+      };
+
+      var query = [];
+      for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+      }
+
+      request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+      request.send(query.join('&'));
+      request = null;
     }
 
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send(query.join('&'));
-    request = null;
-  }
+    var getSelectedText = function() {
+      var selection = window.getSelection();
+      var range = selection.getRangeAt(0);
+      var padding = 30;
+      var leftStartIdx = range.startOffset - padding;
+      var leftEndIdx   = range.startOffset;
+      var left  = (range.startContainer.textContent || "").substring(leftStartIdx, leftEndIdx); 
+      var rightStartIdx = range.endOffset;
+      var rightEndIdx   = range.endOffset + padding;
+      var rightContent  = range.endContainer.textContent || "";
+      var right = rightContent.substring(rightStartIdx, rightEndIdx);
+      return {
+        left: left,
+        m: selection.toString(),
+        right: right
+      }
+    }
+    var getInfo = function() {
+      var title = document.getElementsByTagName("h1")[0] || document.title
+      return {
+        window: JSON.stringify(window.screen),
+        title: title,
+        url: location.href,
+        timestamp: new Date().getTime()
+        }
+    }
+    var onKeyPress = function() {
+      var e = arguments[0] || window.event;
+      var code = e.keyCode ? e.keyCode : (e.which ? e.which : e.charCode);
 
-  var getSelectedText = function() {
-    var selection = window.getSelection();
-    var range = selection.getRangeAt(0);
-    var padding = 30;
-    var leftStartIdx = range.startOffset - padding;
-    var leftEndIdx   = range.startOffset;
-    var left  = (range.startContainer.textContent || "").substring(leftStartIdx, leftEndIdx); 
-    var rightStartIdx = range.endOffset;
-    var rightEndIdx   = range.endOffset + padding;
-    var rightContent  = range.endContainer.textContent || "";
-    var right = rightContent.substring(rightStartIdx, rightEndIdx);
+      if (e.ctrlKey && (code == 13 || code == 10)) {
+        sendReport(getSelectedText(),  getInfo());
+      }
+    }
+
+    var sendReport = function(m, info) {
+      if (m !== undefined) {
+        options.callback();
+        ajaxPost(options.url, {
+          url: info.url,
+          title: info.title,
+          resource: options.resource,
+          m: m.m,
+          left: m.left,
+          right: m.right,
+          timestamp: info.timestamp,
+          options: info.window
+        });
+      }
+    }
+
+    var init = function(opts) {
+      options.url = opts.url || defaultOptions.url;
+      options.callback = opts.callback || defaultOptions.callback;
+      options.minLength = opts.minLength || defaultOptions.minLength;
+      options.maxLength = opts.maxLength || defaultOptions.maxLength;
+      options.resource = opts.resource || defaultOptions.resource;
+      document.onkeypress = onKeyPress;
+    }
+
     return {
-      left: left,
-      emph: selection.toString(),
-      right: right
+      'init': init
     }
-  }
-
-  var onKeyPress = function() {
-    var e = arguments[0] || window.event;
-    var code = e.keyCode ? e.keyCode : (e.which ? e.which : e.charCode);
-
-    if (e.ctrlKey && (code == 13 || code == 10)) {
-    	console.log(getSelectedText())
-        // sendReport(getSelectedText());
-    }
-  }
-
-  var sendReport = function(text) {
-    if (text !== undefined && text.length <= options.maxLength && text.length >= options.minLength) {
-      options.callback();
-
-      ajaxPost(options.url, {misprintText: text, misprintUrl: window.location.href ? window.location.href : '', misprintUserId: options.userId});
-    }
-  }
-
-  var init = function(opts) {
-    options.url = opts.url || defaultOptions.url;
-    options.callback = opts.callback || defaultOptions.callback;
-    options.userId = opts.userId || defaultOptions.userId;
-    options.minLength = opts.minLength || defaultOptions.minLength;
-    options.maxLength = opts.maxLength || defaultOptions.maxLength;
-
-    document.onkeypress = onKeyPress;
-  }
-
-  return {
-    'init': init
-  }
-}();
-try {
-  orphio.init({url: ''})
-} catch (e) {
-  console.log(e);
+  }();
+  try {
+    orphio.init(${data})
+  } catch (e) {
+    console.log(e);
+  }`
 }
